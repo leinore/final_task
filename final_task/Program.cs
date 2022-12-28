d61fae83106ce061e13973879fb7fa677c683e0e
leinore <116540768+leinore@users.noreply.github.com>
2022-12-28 11:57:03 +0300

    final_task

---
+++ final_task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любой текст, слова или знаки через пробел");
+string text = Console.ReadLine();
+string[] Array = text.Split(' ');
+
+Console.WriteLine("Короткие символы в вашем тексте: ");
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+    {
+        Console.WriteLine($"[{Array[i]}]");
+    }
+}
